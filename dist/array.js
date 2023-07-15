13933a45eb96433d3085bdede399cff1dedd5f81
Tarcio Teles <tarcioapps7@gmail.com>
2023-07-15 20:55:14 -0300

    add

--- dist/array.js
+++ dist/array.js
@@ -27,6 +27,13 @@ var alunos2 = alunos.map(function (itemAtual) {
   return itemAtual;
 });
 console.log(alunos2);
+
+//adicionando um elemento ao Array com push.
+
+alunos2.push({
+  nome: 'Tarcinho',
+  curso: 'Back-End'
+});
 var numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 var dobroDosNumeros = numeros.map(function (dobro) {
   dobro = {
@@ -44,3 +51,42 @@ var paula = alunos2.find(function (item) {
 });
 
 console.log(paula);
+
+// Para encontrar o indice da palavra Paula.
+
+var indexDaPaula = alunos2.findIndex(function (item) {
+  return item.nome == 'Paula'; //vai retornar um boolean;
+});
+
+console.log("Esse \xE9 o indice da Palavra Paula: ".concat(indexDaPaula));
+
+//quando o find é inválido retorno undefined
+//findIndex inválido o retorno é 11
+
+//_________________________________________//
+//every: com ele podemos verificar os elementos de um Array, podemos usar para comparar tbm.
+
+var todosAlunosSaoDeFrontEnd = alunos2.every(function (item) {
+  return item.curso === 'Front-End';
+});
+console.log(todosAlunosSaoDeFrontEnd);
+
+//some ele verifica se há pelo menos um elemento no Array.
+
+var existeAlguemDeBackEnd = alunos2.some(function (item) {
+  return item.curso === 'Back-End';
+});
+console.log(existeAlguemDeBackEnd);
+
+//verificar se temos um aluno fullstack que está matriculado no back e no front.
+var fullStack = alunos2.some(function (item) {
+  return item.curso === 'Front-End' && item.curso === 'Back-End';
+});
+console.log(fullStack);
+
+//Para filtrar
+console.log(alunos2);
+var AlunosDeBackEnd = alunos2.filter(function (item) {
+  return item.curso === "Back-End";
+});
+console.log(AlunosDeBackEnd);
--- src/array.js
+++ src/array.js
@@ -28,6 +28,13 @@ const alunos2 = alunos.map(function(itemAtual) {
 console.log(alunos2);
 
 
+//adicionando um elemento ao Array com push.
+
+alunos2. push ({
+    nome: 'Tarcinho',
+    curso: 'Back-End'
+})
+
 const numeros = [1,2,3,4,5,6,7,8,9,10];
 const dobroDosNumeros = numeros.map(function(dobro) {
     dobro = {
@@ -46,3 +53,46 @@ const paula = alunos2.find(function(item){
 
 console.log(paula);
 
+// Para encontrar o indice da palavra Paula.
+
+const indexDaPaula = alunos2.findIndex(function(item){
+    return item.nome == 'Paula'; //vai retornar um boolean;
+})
+
+console.log(`Esse é o indice da Palavra Paula: ${indexDaPaula}`);
+
+//quando o find é inválido retorno undefined
+//findIndex inválido o retorno é 11
+
+//_________________________________________//
+//every: com ele podemos verificar os elementos de um Array, podemos usar para comparar tbm.
+
+const todosAlunosSaoDeFrontEnd = alunos2.every(function(item) {
+    return item.curso === 'Front-End';
+})
+
+console.log(todosAlunosSaoDeFrontEnd);
+
+//some ele verifica se há pelo menos um elemento no Array.
+
+const existeAlguemDeBackEnd = alunos2.some(function (item){
+    return item.curso === 'Back-End';
+})
+
+console.log(existeAlguemDeBackEnd);
+
+//verificar se temos um aluno fullstack que está matriculado no back e no front.
+const fullStack = alunos2.some(function (item) {
+    return item.curso === 'Front-End' && item.curso === 'Back-End';
+})
+
+console.log(fullStack);
+
+//Para filtrar
+
+const AlunosDeBackEnd = alunos2.filter(function(item) {
+    return item.curso === "Back-End";
+})
+
+console.log(alunos2)
+console.log(AlunosDeBackEnd);
